37a111ef3fdad5c8960719ed48b261090be757aa
challavinay251 <challavinay251@gmail.com>
2023-10-10 19:31:39 +0530

    frist commit

---
+++ APP/admin.py
@@ -0,0 +1,357 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from APP.models import four_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','FP','SP','BR','TP','FRP','LP','FTP','STP']
+
+admin.site.register(four_1,TTadmin)
+
+
+from APP.models import three_1
+class TTadmin(admin.ModelAdmin):
+	list_display = ['DAY', 'FP', 'SP', 'BR', 'TP', 'FRP', 'LP', 'FTP', 'STP']
+admin.site.register(three_1, TTadmin);
+
+from APP.models import two_1
+class TTadmin(admin.ModelAdmin):
+	list_display = ['DAY', 'FP', 'SP', 'BR', 'TP', 'FRP', 'LP', 'FTP', 'STP']
+admin.site.register(two_1, TTadmin);
+
+from APP.models import one_1_a
+class TTadmin(admin.ModelAdmin):
+	list_display = ['DAY', 'FP', 'SP', 'BR', 'TP', 'FRP', 'LP', 'FTP', 'STP']
+admin.site.register(one_1_a, TTadmin)
+
+from APP.models import one_1_b
+class TTadmin(admin.ModelAdmin):
+	list_display = ['DAY', 'FP', 'SP', 'BR', 'TP', 'FRP', 'LP', 'FTP', 'STP']
+admin.site.register(one_1_b, TTadmin)
+
+
+from django.contrib import admin
+from APP.models import four_2
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','FP','SP','BR','TP','FRP','LP','FTP','STP']
+
+admin.site.register(four_2,TTadmin)
+
+
+from APP.models import three_2
+class TTadmin(admin.ModelAdmin):
+	list_display = ['DAY', 'FP', 'SP', 'BR', 'TP', 'FRP', 'LP', 'FTP', 'STP']
+admin.site.register(three_2, TTadmin);
+
+from APP.models import two_2
+class TTadmin(admin.ModelAdmin):
+	list_display = ['DAY', 'FP', 'SP', 'BR', 'TP', 'FRP', 'LP', 'FTP', 'STP']
+admin.site.register(two_2, TTadmin);
+
+from APP.models import one_2_a
+class TTadmin(admin.ModelAdmin):
+	list_display = ['DAY', 'FP', 'SP', 'BR', 'TP', 'FRP', 'LP', 'FTP', 'STP']
+admin.site.register(one_2_a, TTadmin);
+
+
+from APP.models import one_2_b
+class TTadmin(admin.ModelAdmin):
+	list_display = ['DAY', 'FP', 'SP', 'BR', 'TP', 'FRP', 'LP', 'FTP', 'STP']
+admin.site.register(one_2_b, TTadmin);
+
+from django.contrib import admin
+from APP.models import Saithirumal_1_1_a
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Saithirumal_1_1_a,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Saithirumal_1_1_b
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Saithirumal_1_1_b,TTadmin)
+
+from django.contrib import admin
+from APP.models import Saithirumal_2_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Saithirumal_2_1,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Saithirumal_2_2
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Saithirumal_2_2,TTadmin)
+
+from django.contrib import admin
+from APP.models import Naveen_1_2_a
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Naveen_1_2_a,TTadmin)
+
+from django.contrib import admin
+from APP.models import Naveen_1_2_b
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Naveen_1_2_b,TTadmin)
+
+from django.contrib import admin
+from APP.models import Naveen_2_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Naveen_2_1,TTadmin)
+
+from django.contrib import admin
+from APP.models import Narender_1_1_a
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Narender_1_1_a,TTadmin)
+
+from django.contrib import admin
+from APP.models import Narender_1_1_b
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Narender_1_1_b,TTadmin)
+
+from django.contrib import admin
+from APP.models import Narender_4_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Narender_4_1,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Babu_2_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Babu_2_1,TTadmin)
+
+from django.contrib import admin
+from APP.models import Babu_2_2
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Babu_2_2,TTadmin)
+
+from django.contrib import admin
+from APP.models import Babu_3_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Babu_3_1,TTadmin)
+
+from django.contrib import admin
+from APP.models import Babu_3_2
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Babu_3_2,TTadmin)
+
+from django.contrib import admin
+from APP.models import Babu_4_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Babu_4_1,TTadmin)
+
+from django.contrib import admin
+from APP.models import Anil_3_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Anil_3_1,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Anil_3_2
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Anil_3_2,TTadmin)
+
+from django.contrib import admin
+from APP.models import Anil_4_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Anil_4_1,TTadmin)
+
+from django.contrib import admin
+from APP.models import Cassian_1_2_a
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Cassian_1_2_a,TTadmin)
+
+from django.contrib import admin
+from APP.models import Cassian_1_2_b
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Cassian_1_2_b,TTadmin)
+
+from django.contrib import admin
+from APP.models import Subharao_1_1_a
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Subharao_1_1_a,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Subharao_1_1_b
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Subharao_1_1_b,TTadmin)
+
+from django.contrib import admin
+from APP.models import Subharao_2_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Subharao_2_1,TTadmin)
+
+from django.contrib import admin
+from APP.models import Subharao_3_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Subharao_3_1,TTadmin)
+
+from django.contrib import admin
+from APP.models import Subharao_3_2
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Subharao_3_2,TTadmin)
+
+from django.contrib import admin
+from APP.models import Subhan_3_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Subhan_3_1,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Subhan_3_2
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Subhan_3_2,TTadmin)
+
+from django.contrib import admin
+from APP.models import Subhan_4_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Subhan_4_1,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Santhosh_3_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Santhosh_3_1,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Santhosh_3_2
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Santhosh_3_2,TTadmin)
+
+from django.contrib import admin
+from APP.models import Devaraj_3_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Devaraj_3_1,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Venkat_1_1_a
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Venkat_1_1_a,TTadmin)
+
+from django.contrib import admin
+from APP.models import Venkat_1_1_b
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Venkat_1_1_b,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Venkat_1_2_a
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Venkat_1_2_a,TTadmin)
+
+from django.contrib import admin
+from APP.models import Venkat_1_2_b
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Venkat_1_2_b,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Zulfiquar_1_1_a
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Zulfiquar_1_1_a,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Zulfiquar_1_1_b
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Zulfiquar_1_1_b,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Zulfiquar_1_2_a
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Zulfiquar_1_2_a,TTadmin)
+
+
+from django.contrib import admin
+from APP.models import Zulfiquar_1_2_b
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Zulfiquar_1_2_b,TTadmin)
+
+from django.contrib import admin
+from APP.models import Zulfiquar_2_1
+class TTadmin(admin.ModelAdmin):
+ list_display=['DAY','Classtime'];
+
+admin.site.register(Zulfiquar_2_1,TTadmin)
